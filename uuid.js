d0fd600bfaa64d52fe17816a11dc784eeaa54287
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2025-04-10 14:00:00 -0300

    Create uuid.js

---
+++ uuid.js
@@ -0,0 +1,21 @@
+function uuid() {
+    let crypto = null
+    if (typeof window === "undefined") {
+        crypto = require("node:crypto")
+    } else {
+        crypto = window.crypto
+    }
+
+    const bytes = new Uint8Array(16)
+
+    crypto.getRandomValues(bytes)
+
+    bytes[6] = (bytes[6] & 0x0f) | 0x40
+    bytes[8] = (bytes[8] & 0x3f) | 0x80
+
+    return Array
+        .from(bytes)
+        .map(byte => byte.toString(16).padStart(2, '0').toUpperCase())
+        .join('')
+        .replace(/^(.{8})(.{4})(.{4})(.{4})(.{12})/, '$1-$2-$3-$4-$5')
+}
